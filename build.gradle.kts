b96e41c59da24f27d6d26bb8731891028a26ac67
Ellis Tsung <git@tsung.co>
2017-11-30 17:02:17 -0500

    I dunno gradle changes?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.junit.platform.gradle.plugin.EnginesExtension
+import org.junit.platform.gradle.plugin.FiltersExtension
+import org.junit.platform.gradle.plugin.JUnitPlatformExtension
 
 version = "1.0-SNAPSHOT"
 
@@ -12,12 +15,13 @@ buildscript {
 
 	dependencies {
 		classpath(kotlinModule("gradle-plugin", kotlin_version))
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.2")
 	}
 
 }
 
 apply {
-//	plugin("java")
+    plugin("org.junit.platform.gradle.plugin")
 	plugin("kotlin")
 }
 
@@ -27,12 +31,30 @@ repositories {
 	mavenCentral()
 }
 
+configure<JUnitPlatformExtension> {
+    filters {
+        engines {
+            include("spek")
+        }
+    }
+}
+
 dependencies {
 	compile(kotlinModule("stdlib-jdk8", kotlin_version))
-//	testCompile("junit", "junit", "4.12")
+    testRuntime(kotlinModule("runtime", kotlin_version))
+	testCompile(kotlinModule("test", kotlin_version))
+    testCompile(kotlinModule("reflect", kotlin_version))
 
-	testCompile("org.jetbrains.kotlin:kotlin-test")
-	testCompile("org.jetbrains.spek:spek-api:1.1.0")
+    testRuntime("org.junit.platform:junit-platform-launcher:1.0.2")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.0.2")
+
+    testCompile ("org.jetbrains.spek:spek-api:1.1.5") {
+        exclude(group="org.jetbrains.kotlin")
+    }
+    testRuntime ("org.jetbrains.spek:spek-junit-platform-engine:1.1.5") {
+        exclude(group="org.junit.platform")
+        exclude(group="org.jetbrains.kotlin")
+    }
 }
 
 configure<JavaPluginConvention> {
@@ -42,3 +64,16 @@ tasks.withType<KotlinCompile> {
 	kotlinOptions.jvmTarget = "1.8"
 }
 
+// extension for configuration
+fun JUnitPlatformExtension.filters(setup: FiltersExtension.() -> Unit) {
+    when (this) {
+        is ExtensionAware -> extensions.getByType(FiltersExtension::class.java).setup()
+        else -> throw Exception("${this::class} must be an instance of ExtensionAware")
+    }
+}
+fun FiltersExtension.engines(setup: EnginesExtension.() -> Unit) {
+    when (this) {
+        is ExtensionAware -> extensions.getByType(EnginesExtension::class.java).setup()
+        else -> throw Exception("${this::class} must be an instance of ExtensionAware")
+    }
+}
