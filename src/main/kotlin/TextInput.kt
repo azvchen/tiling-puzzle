15a271ee1a772426a1b973c3d8b41a14c09c845d
Austin Chen <senseiaustino@gmail.com>
2017-12-05 17:30:48 -0500

    Add code to transform a text input into a list of Tiles

---
+++ src/main/kotlin/TextInput.kt
@@ -0,0 +1,46 @@
+import java.util.*
+
+fun textInput(text: String): List<Tile> {
+    val grid = textToTile(text)
+    val tiles = mutableListOf<Tile>()
+    val visited = mutableSetOf<Pos>()
+    for (r in 0 until grid.width) {
+        for (c in 0 until grid.height) {
+            if (r to c !in visited && grid[r, c] != Tile.blank) {
+                tiles.add(findTile(grid, visited, r, c))
+            }
+        }
+    }
+    return tiles
+}
+
+fun textToTile(text: String): Tile {
+    val grid = mutableMapOf<Pos, Color>()
+    for ((y, line) in text.lines().withIndex()) {
+        for ((x, c) in line.withIndex()) {
+            grid[x to y] = c
+        }
+    }
+    return Tile(grid)
+}
+
+fun findTile(grid: Tile, visited: MutableSet<Pos>, row: Int, col: Int): Tile {
+    val squares = mutableMapOf<Pos, Color>()
+    val queue: Queue<Pos> = ArrayDeque<Pos>()
+    queue.add(row to col)
+
+    while (queue.isNotEmpty()) {
+        val (r, c) = queue.remove()
+        if (r to c in visited) {
+            continue
+        }
+        visited.add(r to c)
+
+        val color = grid[r, c]
+        if (color != null && color != Tile.blank) {
+            squares[r to c] = color
+            queue.addAll(listOf(r-1 to c, r+1 to c, r to c-1, r to c+1))
+        }
+    }
+    return Tile(squares)
+}
---
+++ src/test/kotlin/TextInputSpec.kt
@@ -0,0 +1,61 @@
+import org.amshove.kluent.*
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.xit
+
+object TextInputSpec : Spek({
+    describe("the text to tile-grid converter") {
+        it("should work in a simple case") {
+            val text = "ab\nc"
+            val tile = Tile(mapOf(0 to 0 to 'a', 1 to 0 to 'b', 0 to 1 to 'c'))
+            textToTile(text) shouldEqual tile
+        }
+
+        it("should work with spaces") {
+            val text = " ab\nc d"
+            val tile = Tile(mapOf(1 to 0 to 'a', 2 to 0 to 'b', 0 to 1 to 'c', 2 to 1 to 'd'))
+            textToTile(text) shouldEqual tile
+        }
+    }
+
+    describe("the tile finding algorithm") {
+        val grid = textToTile(listOf(
+            "aa ",
+            "a c",
+            "b c"
+        ).joinToString("\n"))
+
+        it("should work in a simple case") {
+            val visited = mutableSetOf<Pos>()
+            val tile = findTile(grid, visited, 0, 0)
+            tile shouldEqual Tile(mapOf(0 to 0 to 'a', 1 to 0 to 'a', 0 to 1 to 'a', 0 to 2 to 'b'))
+        }
+
+        it("should skip a tile that was visited before") {
+            val visited = mutableSetOf(0 to 0, 1 to 0, 0 to 1, 0 to 2)
+            val tile = findTile(grid, visited, 2, 1)
+            tile shouldEqual Tile(mapOf(0 to 0 to 'c', 0 to 1 to 'c'))
+        }
+    }
+
+    describe("the text input algorithm") {
+        it("should work in a trivial case") {
+            val text = "ab"
+            textInput(text) shouldEqual listOf(Tile(mapOf(0 to 0 to 'a', 1 to 0 to 'b')))
+        }
+
+        it("should work in a simple case") {
+            val text = listOf(
+                "aa ",
+                "a c",
+                "b c"
+            ).joinToString("\n")
+            val tiles = textInput(text)
+            tiles shouldEqual listOf(
+                Tile(mapOf(0 to 0 to 'a', 1 to 0 to 'a', 0 to 1 to 'a', 0 to 2 to 'b')),
+                Tile(mapOf(0 to 0 to 'c', 0 to 1 to 'c'))
+            )
+        }
+    }
+})
